bb5596c3dfdc0011f1d942a6e44d6690045bd02f
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-12-25 19:59:13 +0100

    11 Building Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import apiClient from '../services/api-client';
 import endpoints from '../services/endpoints';
 import { CanceledError } from 'axios';
 
-interface Game {
+export interface Game {
     id: number;
     name: string;
+    background_image: string;
 }
 
 interface FetchGamesResponse {
