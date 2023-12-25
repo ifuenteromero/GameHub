4d824f97c91c7eb37b3254bedc72112b32e7a3ac
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-12-25 16:52:14 +0100

    5 Creating a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import './App.css';
+import Grid from './LibraryComponents/Grid';
+import GridItem from './LibraryComponents/GridItem';
+import IsVisible from './LibraryComponents/IsVisible';
 
 function App() {
-    return <div>Game hub</div>;
+    return (
+        <Grid
+            templateAreas={{
+                base: `"nav" "main"`,
+                lg: `"nav nav" "aside main"`,
+            }}
+        >
+            <GridItem area="nav" bgColor="coral">
+                Nav
+            </GridItem>
+            <IsVisible above="lg">
+                <GridItem area="aside" bgColor="yellow">
+                    Aside
+                </GridItem>
+            </IsVisible>
+            <GridItem area="main" bgColor="dodgerblue">
+                Main
+            </GridItem>
+        </Grid>
+    );
 }
 
 export default App;
