c0611b28d8be0825f557ae753bc68636fcd783e4
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-12-27 13:28:10 +0100

    Icon Component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,41 @@
+import {
+    Icon as ChakraIcon,
+    IconProps as ChakraIconProps,
+} from '@chakra-ui/react';
+import { IconType } from 'react-icons';
+import {
+    FaWindows,
+    FaPlaystation,
+    FaXbox,
+    FaApple,
+    FaLinux,
+    FaAndroid,
+} from 'react-icons/fa';
+
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+
+interface Props extends ChakraIconProps {
+    iconName: string;
+    title: string;
+}
+
+const iconComponents: { [key: string]: IconType } = {
+    FaWindows,
+    FaPlaystation,
+    FaXbox,
+    FaApple,
+    FaLinux,
+    FaAndroid,
+    MdPhoneIphone,
+    SiNintendo,
+    BsGlobe,
+};
+
+const Icon = ({ iconName, ...restProps }: Props) => {
+    const IconComponent = iconComponents[iconName];
+    return <ChakraIcon as={IconComponent} {...restProps} /> || null;
+};
+
+export default Icon;
