35eeae147516962e27d734029f776978f57cb206
Irene Fuente Romero <ifuenteromero@gmail.com>
2023-12-28 19:51:04 +0100

    Fetch the Genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,17 @@
+import useGenres from '../hooks/useGenres';
+
+const GenreList = () => {
+    const { genres, error } = useGenres();
+    return (
+        <>
+            {error && <p>{error}</p>}
+            <ul>
+                {genres.map((genre) => (
+                    <li key={genre.id}>{genre.name}</li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default GenreList;
