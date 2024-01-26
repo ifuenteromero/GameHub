335992ac11fe8dc2df59acee677ef8cfa30e0527
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-01-26 19:52:01 +0100

    Fix the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,14 @@ import { HStack, Img } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
+import { Link } from 'react-router-dom';
+import routes from '../routes';
 
 const NavBar = () => (
     <HStack padding="10px 42px 10px 26px">
-        <Img src={logo} boxSize="60px" />
+        <Link to={routes.root}>
+            <Img src={logo} boxSize="60px" objectFit="cover" />
+        </Link>
         <SearchInput />
         <ColorModeSwitch />
     </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,14 +2,20 @@ import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import useGameQueryStore from '../hooks/useGameQueryStore';
 import Icon from './Icon';
+import { useNavigate } from 'react-router-dom';
+import routes from '../routes';
 
 const SearchInput = () => {
     const inputRef = useRef<HTMLInputElement>(null);
     const onSearch = useGameQueryStore((s) => s.setSearchText);
+    const navigate = useNavigate();
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        if (inputRef.current) onSearch(inputRef.current.value);
+        if (inputRef.current) {
+            onSearch(inputRef.current.value);
+            navigate(routes.root);
+        }
     };
 
     return (
